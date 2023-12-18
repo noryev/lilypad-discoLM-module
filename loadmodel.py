b6cf14d431c75aa7a540b07b1ba924620e27ab9c
noryev <logan.lentz1@gmail.com>
2023-12-18 03:04:39 -0600

    script to interact with model

--- loadmodel.py
+++ loadmodel.py
@@ -5,7 +5,14 @@ def main():
     tokenizer = AutoTokenizer.from_pretrained("huggingface/mistralai/Mixtral-8x7B-Instruct-v0.1")
     model = AutoModelForCausalLM.from_pretrained("huggingface/mistralai/Mixtral-8x7B-Instruct-v0.1")
 
-    # Add your code here to use the model
+  prompt = "Translate the following English text to French: 'Hello, how are you?'"
+
+    inputs = tokenizer(prompt, return_tensors="pt")
+    outputs = model.generate(inputs['input_ids'], max_length=50, num_return_sequences=5)
+    
+    print("Generated texts:")
+    for i, output in enumerate(outputs):
+        print(f"{i}: {tokenizer.decode(output, skip_special_tokens=True)}")
 
 if __name__ == "__main__":
     main()
