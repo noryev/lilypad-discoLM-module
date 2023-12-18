058d1ca14bad33df739e75644fc3648d59aa52b9
noryev <logan.lentz1@gmail.com>
2023-12-18 16:42:40 -0600

    moving to phi-2 to get away from huge tensors

--- loadmodel.py
+++ loadmodel.py
@@ -1,18 +1,16 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
-def main():
-    tokenizer = AutoTokenizer.from_pretrained("DiscoResearch/DiscoLM-mixtral-8x7b-v2")
-    model = AutoModelForCausalLM.from_pretrained("DiscoResearch/DiscoLM-mixtral-8x7b-v2")
+torch.set_default_device("cuda")
 
-    prompt = "Translate the following English text to French: 'Hello, how are you?'"
+model = AutoModelForCausalLM.from_pretrained("microsoft/phi-2", torch_dtype="auto", trust_remote_code=True)
+tokenizer = AutoTokenizer.from_pretrained("microsoft/phi-2", trust_remote_code=True)
 
-    inputs = tokenizer(prompt, return_tensors="pt")
-    outputs = model.generate(inputs['input_ids'], max_length=50, num_return_sequences=5)
-    
-    print("Generated texts:")
-    for i, output in enumerate(outputs):
-        print(f"{i}: {tokenizer.decode(output, skip_special_tokens=True)}")
+inputs = tokenizer('''def print_prime(n):
+   """
+   Print all primes between 1 and n
+   """''', return_tensors="pt", return_attention_mask=False)
 
-if __name__ == "__main__":
-    main()
+outputs = model.generate(**inputs, max_length=200)
+text = tokenizer.batch_decode(outputs)[0]
+print(text)
